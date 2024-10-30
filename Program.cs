8d8260278d02a118d0a9d099eee96559c0dbbbca
Dimitris Gkoris <dimitris@celestialreality.com>
2024-10-30 21:28:25 +0000

    Load all images from a folder

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SharedMemoryManager
 {
@@ -10,6 +8,42 @@ namespace SharedMemoryManager
     {
         static void Main(string[] args)
         {
+            string solutionDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
+            string imageFolderPath = Path.Combine(solutionDirectory, "TestImages");
+
+            var images = LoadImagesFromFolder(imageFolderPath);
+
+            Console.WriteLine($"Loaded {images.Count} images into memory. Press Enter to exit.");
+            Console.ReadLine();
+        }
+
+        static List<byte[]> LoadImagesFromFolder(string folderPath)
+        {
+            var imageList = new List<byte[]>();
+
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine($"Folder doesn't exist: {folderPath}");
+                return imageList;
+            }
+
+            string[] imageFiles = Directory.GetFiles(folderPath, "*.bmp", SearchOption.AllDirectories);
+
+            foreach (var imageFile in imageFiles)
+            {
+                try
+                {
+                    byte[] imageBytes = File.ReadAllBytes(imageFile);
+                    imageList.Add(imageBytes);
+                    Console.WriteLine($"Loaded image: {Path.GetFileName(imageFile)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error loading image {Path.GetFileName(imageFile)}: {ex.Message}");
+                }
+            }
+
+            return imageList;
         }
     }
 }
